4fda241c912fc1d34e5c987d16f60e3c54b9c2e1
xaviergallezot <43139822+xaviergallezot@users.noreply.github.com>
2018-09-24 15:41:51 +0200

    premier commit

---
+++ _gestionBase.inc.php
@@ -0,0 +1,240 @@
+<?php
+
+// FONCTIONS DE CONNEXION
+
+function connect()
+{
+		$hote='mysql:host=localhost;dbname=festival';;
+		$login="festival";
+		$mdp="secret";
+		try
+		{
+		$connexion = new PDO($hote, $login, $mdp, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		}
+		catch (PDOExecption $e) //gestion d'erreur
+		{
+			print "Erreur !:" .$e->getMessage(). "<br/>";
+			die();
+		}
+		return $connexion;
+}
+function selectBase($connexion)
+{
+	$bd="use festival";
+	$query="SET CHARACTER SET utf8";
+	//MOdification du jeu de caracteres de la connexion
+	$res= $connexion->query($query);
+	$res->execute();
+	$ok=$connexion->query($bd);
+	$ok->execute();
+	return $ok;
+}
+// FONCTIONS DE GESTION DES ÉTABLISSEMENTS
+
+function obtenirReqEtablissements()
+{
+   $req="select id, nom from Etablissement order by id";
+   return $req;
+}
+
+function obtenirReqEtablissementsOffrantChambres()
+{
+   $req="select id, nom, nombreChambresOffertes from Etablissement where 
+         nombreChambresOffertes!=0 order by id";
+   return $req;
+}
+
+function obtenirReqEtablissementsAyantChambresAttribuées()
+{
+   $req="select distinct id, nom, nombreChambresOffertes from Etablissement, 
+         Attribution where id = idEtab order by id";
+   return $req;
+}
+
+function obtenirDetailEtablissement($connexion, $id)
+{
+   $req="select * from Etablissement where id='$id'";
+   $rsEtab=$connexion->query($req);
+   return $rsEtab->fetch();
+}
+
+function supprimerEtablissement($connexion, $id)
+{
+   $req="delete from Etablissement where id='$id'";
+   $connexion->query($req);
+}
+ 
+function modifierEtablissement($connexion, $id, $nom, $adresseRue, $codePostal, 
+                               $ville, $tel, $adresseElectronique, $type, 
+                               $civiliteResponsable, $nomResponsable, 
+                               $prenomResponsable, $nombreChambresOffertes)
+{  
+   $nom=str_replace("'", "''", $nom);
+   $adresseRue=str_replace("'","''", $adresseRue);
+   $ville=str_replace("'","''", $ville);
+   $adresseElectronique=str_replace("'","''", $adresseElectronique);
+   $nomResponsable=str_replace("'","''", $nomResponsable);
+   $prenomResponsable=str_replace("'","''", $prenomResponsable);
+  
+   $req="update Etablissement set nom='$nom',adresseRue='$adresseRue',
+         codePostal='$codePostal',ville='$ville',tel='$tel',
+         adresseElectronique='$adresseElectronique',type='$type',
+         civiliteResponsable='$civiliteResponsable',nomResponsable=
+         '$nomResponsable',prenomResponsable='$prenomResponsable',
+         nombreChambresOffertes='$nombreChambresOffertes' where id='$id'";
+   
+   $connexion->query($req);
+}
+
+function creerEtablissement($connexion, $id, $nom, $adresseRue, $codePostal, 
+                            $ville, $tel, $adresseElectronique, $type, 
+                            $civiliteResponsable, $nomResponsable, 
+                            $prenomResponsable, $nombreChambresOffertes)
+{ 
+   $nom=str_replace("'", "''", $nom);
+   $adresseRue=str_replace("'","''", $adresseRue);
+   $ville=str_replace("'","''", $ville);
+   $adresseElectronique=str_replace("'","''", $adresseElectronique);
+   $nomResponsable=str_replace("'","''", $nomResponsable);
+   $prenomResponsable=str_replace("'","''", $prenomResponsable);
+   
+   $req="insert into Etablissement values ('$id', '$nom', '$adresseRue', 
+         '$codePostal', '$ville', '$tel', '$adresseElectronique', '$type', 
+         '$civiliteResponsable', '$nomResponsable', '$prenomResponsable',
+         '$nombreChambresOffertes')";
+   
+   $connexion->query($req);
+}
+
+
+function estUnIdEtablissement($connexion, $id)
+{
+   $req="select * from Etablissement where id='$id'";
+   $rsEtab=$connexion->query($req);
+   return $rsEtab->fetch();
+}
+
+function estUnNomEtablissement($connexion, $mode, $id, $nom)
+{
+   $nom=str_replace("'", "''", $nom);
+   // S'il s'agit d'une création, on vérifie juste la non existence du nom sinon
+   // on vérifie la non existence d'un autre établissement (id!='$id') portant 
+   // le même nom
+   if ($mode=='C')
+   {
+      $req="select * from Etablissement where nom='$nom'";
+   }
+   else
+   {
+      $req="select * from Etablissement where nom='$nom' and id!='$id'";
+   }
+   $rsEtab=$connexion->query($req);
+   return $rsEtab->fetch();
+}
+
+function obtenirNbEtab($connexion)
+{
+   $req="select count(*) as nombreEtab from Etablissement";
+   $rsEtab=$connexion->query($req);
+   $lgEtab=$rsEtab->fetch();
+   return $lgEtab["nombreEtab"];
+}
+
+function obtenirNbEtabOffrantChambres($connexion)
+{
+   $req="select count(*) as nombreEtabOffrantChambres from Etablissement where 
+         nombreChambresOffertes!=0";
+   $rsEtabOffrantChambres=$connexion->query($req);
+   $lgEtabOffrantChambres=$rsEtabOffrantChambres->fetch();
+   return $lgEtabOffrantChambres["nombreEtabOffrantChambres"];
+}
+
+// Retourne false si le nombre de chambres transmis est inférieur au nombre de 
+// chambres occupées pour l'établissement transmis 
+// Retourne true dans le cas contraire
+function estModifOffreCorrecte($connexion, $idEtab, $nombreChambres)
+{
+   $nbOccup=obtenirNbOccup($connexion, $idEtab);
+   return ($nombreChambres>=$nbOccup);
+}
+
+// FONCTIONS RELATIVES AUX GROUPES
+
+function obtenirReqIdNomGroupesAHeberger()
+{
+   $req="select id, nom from Groupe where hebergement='O' order by id";
+   return $req;
+}
+
+function obtenirNomGroupe($connexion, $id)
+{
+   $req="select nom from Groupe where id='$id'";
+   $rsGroupe=$connexion->query($req);
+   $lgGroupe=$rsGroupe->fetch();
+   return $lgGroupe["nom"];
+}
+
+// FONCTIONS RELATIVES AUX ATTRIBUTIONS
+
+// Teste la présence d'attributions pour l'établissement transmis    
+function existeAttributionsEtab($connexion, $id)
+{
+   $req="select * From Attribution where idEtab='$id'";
+   $rsAttrib=$connexion->query($req);
+   return $rsAttrib->fetch();
+}
+
+// Retourne le nombre de chambres occupées pour l'id étab transmis
+function obtenirNbOccup($connexion, $idEtab)
+{
+   $req="select IFNULL(sum(nombreChambres), 0) as totalChambresOccup from
+        Attribution where idEtab='$idEtab'";
+   $rsOccup=$connexion->query($req);
+   $lgOccup=$rsOccup->fetch();
+   return $lgOccup["totalChambresOccup"];
+}
+
+// Met à jour (suppression, modification ou ajout) l'attribution correspondant à
+// l'id étab et à l'id groupe transmis
+function modifierAttribChamb($connexion, $idEtab, $idGroupe, $nbChambres)
+{
+   $req="select count(*) as nombreAttribGroupe from Attribution where idEtab=
+        '$idEtab' and idGroupe='$idGroupe'";
+   $rsAttrib=$connexion->query($req);
+   $lgAttrib=$rsAttrib->fetch();
+   if ($nbChambres==0)
+      $req="delete from Attribution where idEtab='$idEtab' and idGroupe='$idGroupe'";
+   else
+   {
+      if ($lgAttrib["nombreAttribGroupe"]!=0)
+         $req="update Attribution set nombreChambres=$nbChambres where idEtab=
+              '$idEtab' and idGroupe='$idGroupe'";
+      else
+         $req="insert into Attribution values('$idEtab','$idGroupe', $nbChambres)";
+   }
+   $connexion->query($req);
+}
+
+// Retourne la requête permettant d'obtenir les id et noms des groupes affectés
+// dans l'établissement transmis
+function obtenirReqGroupesEtab($id)
+{
+   $req="select distinct id, nom from Groupe, Attribution where 
+        Attribution.idGroupe=Groupe.id and idEtab='$id'";
+   return $req;
+}
+            
+// Retourne le nombre de chambres occupées par le groupe transmis pour l'id étab
+// et l'id groupe transmis
+function obtenirNbOccupGroupe($connexion, $idEtab, $idGroupe)
+{
+   $req="select nombreChambres From Attribution where idEtab='$idEtab'
+        and idGroupe='$idGroupe'";
+   $rsAttribGroupe=$connexion->query($req);
+   if ($lgAttribGroupe=$rsAttribGroupe->fetch())
+      return $lgAttribGroupe["nombreChambres"];
+   else
+      return 0;
+}
+
+?>
---
+++ listeEtablissements.php
@@ -0,0 +1,79 @@
+<?php
+
+include("_debut.inc.php");
+include("_gestionBase.inc.php"); 
+include("_controlesEtGestionErreurs.inc.php");
+
+// CONNEXION AU SERVEUR MYSQL PUIS SÉLECTION DE LA BASE DE DONNÉES festival
+
+$connexion=connect();
+
+if (!$connexion)
+{
+   ajouterErreur("Echec de la connexion au serveur MySql");
+   afficherErreurs();
+   exit();
+}
+if (!selectBase($connexion))
+{
+   ajouterErreur("La base de données festival est inexistante ou non accessible");
+   afficherErreurs();
+   exit();
+}
+// AFFICHER L'ENSEMBLE DES ÉTABLISSEMENTS
+// CETTE PAGE CONTIENT UN TABLEAU CONSTITUÉ D'1 LIGNE D'EN-TÊTE ET D'1 LIGNE PAR
+// ÉTABLISSEMENT
+
+echo "
+<table width='70%' cellspacing='0' cellpadding='0' align='center'
+class='tabNonQuadrille'>
+   <tr class='enTeteTabNonQuad'>
+      <td colspan='4'>Etablissements</td>
+   </tr>";
+     
+   $req=obtenirReqEtablissements();
+   $rsEtab=$connexion->query($req);
+   $lgEtab=$rsEtab->fetch();
+   // BOUCLE SUR LES ÉTABLISSEMENTS
+   while ($lgEtab!=FALSE)
+   {
+      $id=$lgEtab['id'];
+      $nom=$lgEtab['nom'];
+      echo "
+		<tr class='ligneTabNonQuad'>
+         <td width='52%'>$nom</td>
+         
+         <td width='16%' align='center'> 
+         <a href='detailEtablissement.php?id=$id'>
+         Voir détail</a></td>
+         
+         <td width='16%' align='center'> 
+         <a href='modificationEtablissement.php?action=demanderModifEtab&amp;id=$id'>
+         Modifier</a></td>";
+      	
+         // S'il existe déjà des attributions pour l'établissement, il faudra
+         // d'abord les supprimer avant de pouvoir supprimer l'établissement
+			if (!existeAttributionsEtab($connexion, $id))
+			{
+            echo "
+            <td width='16%' align='center'> 
+            <a href='suppressionEtablissement.php?action=demanderSupprEtab&amp;id=$id'>
+            Supprimer</a></td>";
+         }
+         else
+         {
+            echo "
+            <td width='16%'>&nbsp; </td>";          
+			}
+			echo "
+      </tr>";
+      $lgEtab=$rsEtab->fetch();
+   }   
+   echo "
+   <tr class='ligneTabNonQuad'>
+      <td colspan='4'><a href='creationEtablissement.php?action=demanderCreEtab'>
+      Création d'un établissement</a ></td>
+  </tr>
+</table>";
+
+?>
